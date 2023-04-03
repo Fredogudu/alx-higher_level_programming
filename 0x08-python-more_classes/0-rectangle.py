dfd8d174f6634a649ac2fe700c22b80f35fdcb90
fredogudu <fredogudu1@gmail.com>
2023-04-03 08:47:03 -0700

    All task

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on MOn Apr 3, 2023
+@author: Fred Ogudu
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+        empty
+    """
+
+    pass
