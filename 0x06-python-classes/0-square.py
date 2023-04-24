214ccbcc7112784da88f1e2f0b810e4ffd324433
fredogudu <fredogudu1@gmail.com>
2023-04-24 05:06:12 -0700

    added a class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Define a class named Square
+class Square:
+    # The pass statement is used as a placeholder when no code needs to be executed
+    # This is used to indicate that the class has no methods or attributes defined yet
+    pass
+
